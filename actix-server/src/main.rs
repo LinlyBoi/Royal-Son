83b76ee9d40680059683b96e1525799c44f9617b
LinlyBoi <libkyy@e.email>
2023-02-12 23:33:27 +0200

    i did sth here idk )

--- actix-server/src/main.rs
+++ actix-server/src/main.rs
@@ -1,34 +1,43 @@
 use actix_files::Files;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
-use sqlx::postgres::PgPoolOptions;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use std::env;
 
+mod pog;
+mod queries;
 #[cfg(test)]
 mod tests;
-
-pub mod pog;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let port: u16 = env::var("ACTIX_PORT")
-        .expect("SET ACTIX_PORT PLOX")
-        .parse()
-        .expect("NOPE CANT PARSE THIS WHAT DID YOU PUT IN?!!!");
-    let address = env::var("ACTIX_IP").expect("SET ACTIX_IP PLOX");
-    let database_url = env::var("DATABASE_URL").expect("Put a DB url in the .env file dumbass");
-    let pool = PgPoolOptions::new()
-        .max_connections(10)
-        .connect(database_url.as_str())
-        .await
-        .expect("No pool connection man :(");
+    let (port, address) = init_address();
     HttpServer::new(move || {
         App::new()
-            .app_data(web::Data::new(pool.clone()))
+            .app_data(web::Data::new(init_pool()))
             .service(Files::new("/", "./dist/").index_file("index.html"))
     })
     .bind((address.as_str(), port))?
     .run()
     .await
+}
+
+fn init_address() -> (u16, String) {
+    let port: u16 = env::var("ACTIX_PORT")
+        .expect("SET ACTIX_PORT PLOX")
+        .parse()
+        .expect("NOPE CANT PARSE THIS WHAT DID YOU PUT IN?!!!");
+    let address = env::var("ACTIX_IP").expect("SET ACTIX_IP PLOX");
+    (port, address)
+}
+
+//AAA sync -> Database pools YEAA
+async fn init_pool() -> Pool<Postgres> {
+    let database_url = env::var("DATABASE_URL").expect("Put a DB url in the .env file dumbass");
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(database_url.as_str())
+        .await
+        .expect("No pool connection man :(")
 }
