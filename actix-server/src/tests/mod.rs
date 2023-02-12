81c58f48dd6586ed280149ef65b4a4eaa4840095
LinlyBoi <libkyy@e.email>
2023-02-12 23:33:20 +0200

    tests for the module yes (Mjokr your balls)

---
+++ actix-server/src/tests/mod.rs
@@ -0,0 +1,15 @@
+use dotenv::dotenv;
+
+use crate::{init_pool, queries};
+#[actix_web::test]
+async fn fetch_complaints() {
+    dotenv().ok();
+    //here there are at least 2 queries in the table
+    let q1 = queries::fetch_complaints(1, init_pool().await).await;
+    let q2 = queries::fetch_complaints(2, init_pool().await).await;
+    println!("{}", q1.len());
+    println!("{}", q2.len());
+    assert_eq!(1, q1.len());
+    assert_eq!(2, q2.len());
+    assert_eq!("Student injury", q1[0].category)
+}
